960a3d4caa373e46433e3f393aaec751c348bed9
rama_qandel <rama.qandel@joacademy.com>
2024-06-05 17:38:33 +0300

    fixed

--- pages/files/[slug].tsx
+++ pages/files/[slug].tsx
@@ -16,7 +16,7 @@ export const getServerSideProps = async (
       "https://admin.joacademy.net/api/v1/get-files-by-needle",
       {
         // @ts-ignore
-        params: { needle: encodeURIComponent(query?.slug).replace(/\+/g, '%2B') },
+        params: { needle: decodeURI(query?.slug).replace(/\+/g, '%2B') },
         headers: {
           lang: locale ?? "en",
         },
@@ -45,7 +45,7 @@ const File = ({ payload }: any) => {
   console.log(payload, "payload");
   console.log(router, "router");
           // @ts-ignore
-console.log(encodeURIComponent(router?.query?.slug).replace(/\+/g, '%2B'))
+console.log(decodeURI(router?.query?.slug).replace(/\+/g, '%2B'))
 
   return (
     <>
